f4774a73dbb24c462e6b2f1012b1387c3339c5a7
brainzz <44133558+brainzz2285@users.noreply.github.com>
2018-10-14 15:50:54 +0530

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -6,17 +6,17 @@ int G[10][10], visited[10], n;	//n is no of vertices and graph is sorted in arra
 void main()
 {
   int i, j;
-  printf ("Enter number of vertices:");
-
-  scanf ("%d", &n);
-
-  //read the adjecency matrix
-  printf ("\nEnter adjecency matrix of the graph:");
-
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
-      scanf ("%d", &G[i][j]);
-
+  n=8;
+  int G[n][n] = { {0,1,1,1,1,0,0,0},
+  {1,0,0,0,0,1,0,0},
+  {1,0,0,0,0,1,0,0},
+  {1,0,0,0,0,0,1,0},
+  {1,0,0,0,0,0,1,0},
+  {0,1,1,0,0,0,0,1},
+  {0,0,0,1,1,0,0,1},
+  {0,0,0,0,0,1,1,0}
+};
+ 
   //visited is initialized to zero
   for (i = 0; i < n; i++)
     visited[i] = 0;
