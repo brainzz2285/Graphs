d34be2f44f96975e940aa6ebba2420e7d64fa8ec
brainzz <44133558+brainzz2285@users.noreply.github.com>
2018-10-14 15:56:53 +0530

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 
 void DFS (int);
-int G[10][10], visited[10], n;	//n is no of vertices and graph is sorted in array G[10][10]
+int visited[10],n=8;	//n is no of vertices and graph is sorted in array G[10][10]
 
-void main()
-{
-  int i, j;
-  n=8;
-  int G[n][n] = { {0,1,1,1,1,0,0,0},
+// see dfsc.jpg to understand the below adjacency matrix
+ int G[8][8] = { {0,1,1,1,1,0,0,0},
   {1,0,0,0,0,1,0,0},
   {1,0,0,0,0,1,0,0},
   {1,0,0,0,0,0,1,0},
@@ -16,11 +13,14 @@ void main()
   {0,0,0,1,1,0,0,1},
   {0,0,0,0,0,1,1,0}
 };
- 
+
+
+void main()
+{
+  int i, j;
   //visited is initialized to zero
   for (i = 0; i < n; i++)
     visited[i] = 0;
-
   DFS (0);
 }
 
@@ -29,7 +29,6 @@ void DFS(int i)
   int j;
   printf ("\n%d", i);
   visited[i] = 1;
-
   for (j = 0; j < n; j++)
     if (!visited[j] && G[i][j] == 1)
       DFS (j);
